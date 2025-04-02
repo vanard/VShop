4eb9f8caa01e9dc7d5f7bf02d28cb8db91cf2e1d
vanard <vianixa34@gmail.com>
2025-04-02 15:52:12 +0800

    add font family

---
+++ app/src/main/java/com/vanard/vshop/common/FontTypography.kt
@@ -0,0 +1,19 @@
+package com.vanard.vshop.common
+
+import androidx.compose.ui.text.font.Font
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
+import com.vanard.vshop.R
+import androidx.compose.material3.Typography
+
+val poppinsFamily = FontFamily(
+    Font(R.font.poppins_regular, FontWeight.Normal),
+    Font(R.font.poppins_semibold, FontWeight.SemiBold),
+    Font(R.font.poppins_bold, FontWeight.Bold),
+)
+
+val encodeSansFamily = FontFamily(
+    Font(R.font.encode_sans_regular, FontWeight.Normal),
+    Font(R.font.encode_sans_semibold, FontWeight.SemiBold),
+    Font(R.font.encode_sans_bold, FontWeight.Bold),
+)
