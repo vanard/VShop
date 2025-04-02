390e995c76d42e382049d7b549a18a5a4fa6731c
vanard <vianixa34@gmail.com>
2025-04-02 15:51:50 +0800

    setup splash and application class

---
+++ app/src/main/java/com/vanard/vshop/persentation/AppController.kt
@@ -0,0 +1,13 @@
+package com.vanard.vshop.persentation
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class AppController: Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+    }
+
+}
