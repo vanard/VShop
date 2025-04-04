23486f9fe8b8e3494f853e18819e56c28591343c
vanard <vianixa34@gmail.com>
2025-04-04 20:41:43 +0800

    update api interface

--- app/src/main/java/com/vanard/vshop/data/remote/ApiInterface.kt
+++ app/src/main/java/com/vanard/vshop/data/remote/ApiInterface.kt
@@ -1,4 +1,24 @@
 package com.vanard.vshop.data.remote
 
+import kotlinx.coroutines.flow.Flow
+import okhttp3.Route
+import retrofit2.Response
+import retrofit2.http.GET
+import retrofit2.http.Path
+
+const val PRODUCTS_BY_CATEGORY_ENDPOINT = "products/category/{category}"
+const val PRODUCT_BY_ID_ENDPOINT = "products/{id}"
+const val PRODUCTS_ENDPOINT = "products"
+
 interface ApiInterface {
+
+    @GET(PRODUCTS_ENDPOINT)
+    suspend fun getAllProducts(): Flow<Response<ProductList>>
+
+    @GET(PRODUCT_BY_ID_ENDPOINT)
+    suspend fun getProductById(@Path(RouteConstant.ID) id: Int): Response<Product>
+
+    @GET(PRODUCTS_BY_CATEGORY_ENDPOINT)
+    suspend fun getProductsByCategory(@Path(RouteConstant.CATEGORY) category: String): Response<ProductList>
+
 }
