97a82ddc9bb6e9c2f1ad95a25f6f7049b536b78f
vanard <vianixa34@gmail.com>
2025-04-05 22:21:25 +0800

    update api service and add chucker

--- app/src/main/java/com/vanard/vshop/data/remote/ApiService.kt
+++ app/src/main/java/com/vanard/vshop/data/remote/ApiService.kt
@@ -1,7 +1,6 @@
 package com.vanard.vshop.data.remote
 
 import kotlinx.coroutines.flow.Flow
-import okhttp3.Route
 import retrofit2.Response
 import retrofit2.http.GET
 import retrofit2.http.Path
@@ -10,15 +9,17 @@ const val PRODUCTS_BY_CATEGORY_ENDPOINT = "products/category/{category}"
 const val PRODUCT_BY_ID_ENDPOINT = "products/{id}"
 const val PRODUCTS_ENDPOINT = "products"
 
-interface ApiInterface {
+interface ApiService {
 
     @GET(PRODUCTS_ENDPOINT)
-    suspend fun getAllProducts(): Flow<Response<ProductList>>
+    suspend fun getAllProducts(): Response<List<Product>>
+//    suspend fun getAllProducts(): Response<ProductList>
 
     @GET(PRODUCT_BY_ID_ENDPOINT)
     suspend fun getProductById(@Path(RouteConstant.ID) id: Int): Response<Product>
 
     @GET(PRODUCTS_BY_CATEGORY_ENDPOINT)
-    suspend fun getProductsByCategory(@Path(RouteConstant.CATEGORY) category: String): Response<ProductList>
+    suspend fun getProductsByCategory(@Path(RouteConstant.CATEGORY) category: String): Response<List<Product>>
+//    suspend fun getProductsByCategory(@Path(RouteConstant.CATEGORY) category: String): Response<ProductList>
 
 }
--- app/src/main/java/com/vanard/vshop/di/NetworkModule.kt
+++ app/src/main/java/com/vanard/vshop/di/NetworkModule.kt
@@ -1,13 +1,20 @@
 package com.vanard.vshop.di
 
-import com.vanard.vshop.data.remote.ApiInterface
+import android.content.Context
+import com.chuckerteam.chucker.api.ChuckerInterceptor
+import com.squareup.moshi.Moshi
+import com.vanard.vshop.data.remote.ApiService
 import com.vanard.vshop.network.BASE_URL
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
 import dagger.hilt.components.SingletonComponent
+import okhttp3.OkHttpClient
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
+import retrofit2.converter.moshi.MoshiConverterFactory
+import java.util.concurrent.TimeUnit
 import javax.inject.Singleton
 
 @Module
@@ -20,14 +27,41 @@ object NetworkModule {
 
     @Singleton
     @Provides
-    fun getRetrofitClient(baseUrl: String): Retrofit =
-        Retrofit.Builder()
-            .baseUrl(baseUrl)
-            .addConverterFactory(GsonConverterFactory.create())
-            .build()
+    fun getOkHttpClient(@ApplicationContext context: Context): OkHttpClient {
+        val okHttpClient = OkHttpClient.Builder()
+        okHttpClient.addInterceptor(ChuckerInterceptor(context))
+        okHttpClient.callTimeout(10, TimeUnit.SECONDS)
+        okHttpClient.connectTimeout(10, TimeUnit.SECONDS)
+        okHttpClient.writeTimeout(10, TimeUnit.SECONDS)
+        okHttpClient.readTimeout(10, TimeUnit.SECONDS)
+        val okHttp = okHttpClient.build()
+        return okHttp
+    }
+
+//    @Provides
+//    @Singleton
+//    fun provideMoshiConverter(): MoshiConverterFactory {
+//        val moshi = Moshi.Builder().add(KotlinJsonAdapterFactory()).build()
+//
+//        return
+//    }
 
     @Singleton
     @Provides
-    fun getApiClient(retrofit: Retrofit): ApiInterface = retrofit.create(ApiInterface::class.java)
+    fun getRetrofit(baseUrl: String, okHttpClient: OkHttpClient): Retrofit =
+        Retrofit.Builder()
+            .baseUrl(baseUrl)
+//            .addConverterFactory(MoshiConverterFactory.create())
+            .addConverterFactory(GsonConverterFactory.create())
+            .client(okHttpClient)
+            .build()
+
+//    @Singleton
+//    @Provides
+//    fun getApiProductService(retrofit: Retrofit): ApiService = retrofit.create(ApiService::class.java)
+
+    @Singleton
+    @Provides
+    fun getApiService(retrofit: Retrofit): ApiService = retrofit.create(ApiService::class.java)
 
 }
