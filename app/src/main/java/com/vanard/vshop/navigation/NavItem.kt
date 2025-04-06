68ebd3d62743e801c3a665932b5e08ed57cda463
vanard <vianixa34@gmail.com>
2025-04-06 16:02:24 +0800

    add getAllNavigationItem()

--- app/src/main/java/com/vanard/vshop/navigation/NavItem.kt
+++ app/src/main/java/com/vanard/vshop/navigation/NavItem.kt
@@ -1,6 +1,9 @@
 package com.vanard.vshop.navigation
 
+import androidx.compose.runtime.Composable
 import androidx.compose.ui.graphics.painter.Painter
+import androidx.compose.ui.res.painterResource
+import com.vanard.vshop.R
 
 data class NavItem(
 //    val title: String,
@@ -8,3 +11,29 @@ data class NavItem(
     val selectedIcon: Painter,
     val screen: Screen
 )
+
+@Composable
+fun getAllNavigationItem(): List<NavItem> {
+    return listOf(
+        NavItem(
+            icon = painterResource(R.drawable.home_2),
+            selectedIcon = painterResource(R.drawable.home_2_bold),
+            screen = Screen.Home
+        ),
+        NavItem(
+            icon = painterResource(R.drawable.heart),
+            selectedIcon = painterResource(R.drawable.heart_bold),
+            screen = Screen.Whishlist
+        ),
+        NavItem(
+            icon = painterResource(R.drawable.shopping_bag),
+            selectedIcon = painterResource(R.drawable.shopping_bag_bold),
+            screen = Screen.Cart
+        ),
+        NavItem(
+            icon = painterResource(R.drawable.profile),
+            selectedIcon = painterResource(R.drawable.profile_bulk),
+            screen = Screen.Profile
+        ),
+    )
+}
