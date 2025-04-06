c7c7c4d7f96babf7a58fc40a1efa59e97db551b9
vanard <vianixa34@gmail.com>
2025-04-06 16:02:44 +0800

    add dummy product

--- app/src/main/java/com/vanard/vshop/data/remote/Product.kt
+++ app/src/main/java/com/vanard/vshop/data/remote/Product.kt
@@ -16,3 +16,13 @@ data class Rating(
     val rate: Double,
     val count: Long,
 )
+
+val dummyProduct = Product(
+    0,
+    "Title Product",
+    219.30,
+    "Dummy Product",
+    "Dummy",
+    image = "https://blog.sribu.com/wp-content/uploads/2024/10/t-shirt-7973405_1280.jpg",
+    rating = Rating(2.9, 14)
+)
