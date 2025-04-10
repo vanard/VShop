e4acbff23ea61c1c879d11e17c50943812bb6b26
vanard <vianixa34@gmail.com>
2025-04-10 16:11:26 +0800

    store resource to variable

--- core/ui/src/main/java/com/vanard/ui/components/CategoryItem.kt
+++ core/ui/src/main/java/com/vanard/ui/components/CategoryItem.kt
@@ -1,4 +1,4 @@
-package com.vanard.vshop.persentation.components
+package com.vanard.ui.components
 
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
@@ -16,8 +16,8 @@ import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import com.vanard.vshop.R
-import com.vanard.vshop.persentation.ui.theme.VShopTheme
+import com.vanard.resources.R
+import com.vanard.ui.theme.VShopTheme
 
 @Composable
 fun CategoryItemContent(
@@ -26,27 +26,32 @@ fun CategoryItemContent(
     modifier: Modifier = Modifier,
 ) {
     val shapeBg = RoundedCornerShape(8.dp)
+    val colorBorder = if (isSelected)
+        colorResource(R.color.paint_01)
+    else colorResource(R.color.paint_03)
+    val colorBg = if (isSelected)
+        colorResource(R.color.paint_01)
+    else
+        Color.Transparent
+
+    val rowModifier = modifier
+        .toggleable(value = isSelected, onValueChange = {
+            onSelectionChanged(category)
+        })
+        .border(
+            1.dp,
+            color = colorBorder,
+            shape = shapeBg
+        )
+        .background(
+            color = colorBg,
+            shape = shapeBg
+        )
+        .padding(12.dp)
+
     Surface(shadowElevation = 4.dp, modifier = modifier.clip(shapeBg)) {
         Row(
-            modifier = modifier
-                .toggleable(value = isSelected, onValueChange = {
-                    onSelectionChanged(category)
-                })
-                .border(
-                    1.dp,
-                    color = if (isSelected)
-                        colorResource(R.color.paint_01)
-                    else colorResource(R.color.paint_03),
-                    shape = shapeBg
-                )
-                .background(
-                    color = if (isSelected)
-                        colorResource(R.color.paint_01)
-                    else
-                        Color.Transparent,
-                    shape = shapeBg
-                )
-                .padding(12.dp)
+            modifier = rowModifier
         ) {
             Text(
                 fontSize = 12.sp, text = category, color = if (isSelected)
