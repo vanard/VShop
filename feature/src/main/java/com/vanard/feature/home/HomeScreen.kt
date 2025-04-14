52aaca3c4daffbb5337effc399d2f5aa5bff31ee
vanard <vianixa34@gmail.com>
2025-04-14 16:33:57 +0800

    add function button sort and categories, add animation grid list

--- feature/src/main/java/com/vanard/feature/home/HomeScreen.kt
+++ feature/src/main/java/com/vanard/feature/home/HomeScreen.kt
@@ -1,18 +1,16 @@
 package com.vanard.feature.home
 
-import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.animation.core.tween
+import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
-import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.rememberLazyListState
@@ -22,8 +20,6 @@ import androidx.compose.foundation.lazy.staggeredgrid.items
 import androidx.compose.foundation.lazy.staggeredgrid.rememberLazyStaggeredGridState
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.foundation.text.BasicTextField
-import androidx.compose.foundation.text.input.TextFieldState
 import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
@@ -31,6 +27,8 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.runtime.withFrameNanos
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
@@ -39,7 +37,6 @@ import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -54,7 +51,10 @@ import com.vanard.ui.components.ChipGroup
 import com.vanard.ui.components.CustomSearchBar
 import com.vanard.ui.components.ShopItemContent
 import com.vanard.ui.theme.VShopTheme
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 
+@OptIn(ExperimentalFoundationApi::class)
 @Composable
 fun HomeScreen(
     modifier: Modifier = Modifier,
@@ -69,12 +69,21 @@ fun HomeScreen(
 
             is UIState.Success -> {
                 val context = LocalContext.current
+                val coroutineScope = rememberCoroutineScope()
                 val scrollState = rememberLazyListState()
                 val scrollGridState = rememberLazyStaggeredGridState()
 
                 val searchText by viewModel.searchText.collectAsState()
                 val products by viewModel.products.collectAsState()
                 val isSearching by viewModel.isSearching.collectAsState()
+
+                fun scrollToTop() {
+                    coroutineScope.launch {
+//                            delay(10)
+                        withFrameNanos { }
+                        scrollGridState.scrollToItem(0)
+                    }
+                }
 
                 Column(
                     modifier = modifier
@@ -84,7 +93,7 @@ fun HomeScreen(
                     Row(modifier = modifier.padding(top = 20.dp)) {
                         Column(
                             modifier = modifier
-                                .weight(1f, fill = false)
+                                .weight(1f)
                                 .padding(end = 24.dp)
                         ) {
                             Text(
@@ -94,7 +103,7 @@ fun HomeScreen(
                             )
                             Spacer(Modifier.size(8.dp))
                             Text(
-                                text = "User Display Name ALJFIOSDJFOIDSJFLQj /n sfkejj kladf asdfk",
+                                text = "User Guest",
                                 fontSize = 16.sp,
                                 fontWeight = FontWeight.SemiBold,
                                 modifier = modifier.weight(1f, fill = false)
@@ -117,37 +126,6 @@ fun HomeScreen(
                         verticalAlignment = Alignment.CenterVertically,
                         modifier = modifier
                     ) {
-//                        Row(
-//                            horizontalArrangement = Arrangement.Center,
-//                            modifier = modifier
-//                                .padding(end = 16.dp)
-//                                .weight(1f)
-//                                .border(
-//                                    1.dp,
-//                                    color = colorResource(R.color.paint_02),
-//                                    shape = RoundedCornerShape(8.dp)
-//                                )
-//                                .padding(16.dp)
-//                        ) {
-//                            Icon(
-//                                painter = painterResource(R.drawable.search_normal),
-//                                tint = Color.Black,
-//                                contentDescription = null,
-//                                modifier = modifier
-//                            )
-//                            BasicTextField(
-//                                TextFieldState(""),
-//                                textStyle = TextStyle(fontSize = 16.sp),
-//                                modifier = modifier.weight(1f)
-//                            )
-//                            AnimatedVisibility(visible = true) {
-//                                Icon(
-//                                    painter = painterResource(R.drawable.setting_4),
-//                                    tint = Color.Black,
-//                                    contentDescription = null
-//                                )
-//                            }
-//                        }
                         CustomSearchBar(
                             query = searchText,
                             onQueryChanged = viewModel::onSearchTextChange,
@@ -157,6 +135,8 @@ fun HomeScreen(
                         IconButton(
                             onClick = {
                                 context.toastMsg("Sort Click")
+                                viewModel.sortProducts()
+                                scrollToTop()
                             },
                             modifier = modifier
                                 .padding(start = 16.dp)
@@ -178,9 +158,14 @@ fun HomeScreen(
                         selectedCategories = viewModel.selectedCategory.value,
                         onSelectedChanged = {
                             viewModel.selectCategory(getCategories(it))
+//                            coroutineScope.launch {
+//                                delay(10)
+//                                scrollGridState.scrollToItem(0)
+//                            }
+                            scrollToTop()
                         })
 
-                    if (isSearching){
+                    if (isSearching) {
                         Box(modifier = Modifier.fillMaxSize()) {
                             CircularProgressIndicator(
                                 modifier = Modifier.align(Alignment.Center)
@@ -198,11 +183,18 @@ fun HomeScreen(
                             items(
                                 items = products.products,
                                 key = { it.id }) { product ->
-                                ShopItemContent(product, {
-                                    context.toastMsg("Product ${product.title}")
-                                }, {
-                                    context.toastMsg("${product.title} Fav")
-                                })
+                                ShopItemContent(
+                                    product,
+                                    onSelectedProduct = {
+                                        context.toastMsg("Product ${product.title}")
+                                    },
+                                    onFavClick = {
+                                        context.toastMsg("${product.title} Fav")
+                                    },
+                                    modifier = modifier.animateItemPlacement(
+                                        animationSpec = tween(300)
+                                    )
+                                )
                             }
                         }
                     )
