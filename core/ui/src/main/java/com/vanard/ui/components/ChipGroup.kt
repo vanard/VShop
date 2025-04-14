e6137e5994298baafab25fd43f0c157403d57741
vanard <vianixa34@gmail.com>
2025-04-14 15:39:16 +0800

    add lazy row state on chip group

--- core/ui/src/main/java/com/vanard/ui/components/ChipGroup.kt
+++ core/ui/src/main/java/com/vanard/ui/components/ChipGroup.kt
@@ -2,8 +2,10 @@ package com.vanard.ui.components
 
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.lazy.LazyListState
 import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.lazy.rememberLazyListState
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -13,11 +15,13 @@ import com.vanard.domain.model.getAllCategories
 @Preview(showBackground = true)
 @Composable
 fun ChipGroup(
+    state: LazyListState = rememberLazyListState(),
     categories: List<Categories> = getAllCategories(),
     selectedCategories: Categories? = Categories.AllItems,
     onSelectedChanged: (String) -> Unit = {},
 ) {
     LazyRow(
+        state = state,
         contentPadding = PaddingValues(vertical = 20.dp),
         horizontalArrangement = Arrangement.spacedBy(16.dp)
     ) {
