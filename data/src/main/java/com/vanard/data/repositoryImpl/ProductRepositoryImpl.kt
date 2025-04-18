4d069c9a667900796e10a2f81f135fde504a091a
vanard <vianixa34@gmail.com>
2025-04-18 14:57:39 +0800

    refactor getAllProducts add network bound resource

--- data/src/main/java/com/vanard/data/repositoryImpl/ProductRepositoryImpl.kt
+++ data/src/main/java/com/vanard/data/repositoryImpl/ProductRepositoryImpl.kt
@@ -1,25 +1,40 @@
 package com.vanard.data.repositoryImpl
 
-import com.vanard.core.common.UIState
+import com.vanard.common.UIState
+import com.vanard.common.util.networkBoundResourceUiState
+import com.vanard.common.util.safeBodyOrThrow
 import com.vanard.core.common.fetchState
 import com.vanard.core.common.map
+import com.vanard.data.dao.ProductDao
 import com.vanard.data.mappers.toDomain
+import com.vanard.data.mappers.toEntity
 import com.vanard.data.remote.ProductService
 import com.vanard.domain.model.Product
 import com.vanard.domain.repository.ProductRepository
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.flow.map
 import javax.inject.Inject
 
-class ProductRepositoryImpl @Inject constructor(private val productServices: ProductService) :
+class ProductRepositoryImpl @Inject constructor(private val productServices: ProductService, private val productDao: ProductDao) :
     ProductRepository {
 
-    override suspend fun getAllProducts(): Flow<UIState<List<Product>>> = flow {
-        val result = fetchState { productServices.getAllProducts() }
-        emit(result.map { dtoList ->
-            dtoList.map { it.toDomain() }
-        })
-    }
+    override suspend fun getAllProducts(): Flow<UIState<List<Product>>> =
+        networkBoundResourceUiState(
+            query = {
+                productDao.getAllProducts().map { list -> list.map { it.toDomain() } }
+            },
+            fetch = {
+                productServices.getAllProducts().safeBodyOrThrow()
+            },
+            saveFetchResult = { dtoList ->
+                productDao.deleteAllProducts() // Optional: see earlier notes
+                productDao.insertAllProduct(dtoList.map { it.toEntity() })
+            },
+            shouldFetch = { localData ->
+                localData.isEmpty()
+            }
+        )
 
     override suspend fun getProductById(id: Int): Flow<UIState<Product>> = flow {
         val result = fetchState { productServices.getProductById(id) }
