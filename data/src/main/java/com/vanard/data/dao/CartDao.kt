f1724f8a08ace61622adae6b007e212d9aa143d7
vanard <vianixa34@gmail.com>
2025-04-18 19:30:13 +0800

    add cart dao

---
+++ data/src/main/java/com/vanard/data/dao/CartDao.kt
@@ -0,0 +1,32 @@
+package com.vanard.data.dao
+
+import androidx.room.Dao
+import androidx.room.Delete
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+import com.vanard.data.entities.CartEntity
+import kotlinx.coroutines.flow.Flow
+
+@Dao
+interface CartDao {
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insertCart(cartEntity: CartEntity)
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insertAllCart(listCartEntity: List<CartEntity>)
+
+    @Query("SELECT * FROM carts_item WHERE id = :id")
+    fun getCartById(id: Long): Flow<CartEntity?>
+
+    @Query("SELECT * FROM carts_item")
+    fun getAllCarts(): Flow<List<CartEntity>>
+
+    @Delete
+    suspend fun deleteCart(productEntity: CartEntity)
+
+    @Query("DELETE FROM carts_item")
+    suspend fun deleteAllCarts()
+
+}
--- data/src/main/java/com/vanard/data/db/AppDatabase.kt
+++ data/src/main/java/com/vanard/data/db/AppDatabase.kt
@@ -2,10 +2,18 @@ package com.vanard.data.db
 
 import androidx.room.Database
 import androidx.room.RoomDatabase
+import com.vanard.data.dao.CartDao
 import com.vanard.data.dao.ProductDao
+import com.vanard.data.entities.CartEntity
 import com.vanard.data.entities.ProductEntity
 
-@Database(entities = [ProductEntity::class], version = 1, exportSchema = false)
+@Database(
+    entities = [ProductEntity::class, CartEntity::class],
+    version = 1,
+    exportSchema = false
+)
+
 abstract class AppDatabase : RoomDatabase() {
     abstract fun ProductDao(): ProductDao
+    abstract fun CartDao(): CartDao
 }
