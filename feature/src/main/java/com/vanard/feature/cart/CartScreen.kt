f78ca34de3c9cda5362d124fb3e9064d0a5431ac
vanard <vianixa34@gmail.com>
2025-04-18 14:52:11 +0800

    add cart to cart screen

--- feature/src/main/java/com/vanard/feature/cart/CartScreen.kt
+++ feature/src/main/java/com/vanard/feature/cart/CartScreen.kt
@@ -6,7 +6,10 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.sizeIn
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.lazy.rememberLazyListState
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material3.Button
 import androidx.compose.material3.HorizontalDivider
@@ -14,8 +17,12 @@ import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalConfiguration
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
@@ -24,7 +31,9 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.hilt.navigation.compose.hiltViewModel
+import com.vanard.common.util.toastMsg
 import com.vanard.resources.R
+import com.vanard.ui.components.CartItemContent
 import com.vanard.ui.theme.VShopTheme
 
 @Composable
@@ -34,6 +43,13 @@ fun CartScreen(
 ) {
     var total = 0
     viewModel.getCarts()
+
+    val configuration = LocalConfiguration.current
+    val screenHeight = configuration.screenHeightDp.dp
+
+    val context = LocalContext.current
+    val carts by viewModel.carts.collectAsState()
+    val scrollState = rememberLazyListState()
 
     Column(
         modifier = modifier
@@ -83,24 +99,50 @@ fun CartScreen(
 
         }
 
-        LazyColumn(modifier = modifier) {
+        LazyColumn(
+            state = scrollState,
+            modifier = modifier.sizeIn(maxHeight = (screenHeight / 2) - 48.dp),
+            content = {
+                items(
+                    items = carts.carts,
+                    key = { it.id }) { cart ->
+                    CartItemContent(
+                        cart,
+                        onSelectedProduct = {
+                            context.toastMsg("Product Cart")
+                        },
+                    )
+                }
 
-        }
+            })
 
-        Text("Shipping Information", fontWeight = FontWeight.SemiBold)
-        Row(modifier = modifier.padding(top = 16.dp)) {
+        Text(
+            "Shipping Information",
+            fontWeight = FontWeight.SemiBold,
+            modifier = modifier.padding(top = 16.dp)
+        )
+
+        Row(modifier = modifier.padding(top = 12.dp)) {
             Text("Total", modifier = modifier.weight(1f))
             Text("$10003", fontWeight = FontWeight.SemiBold)
         }
+
         Row(modifier = modifier.padding(top = 8.dp)) {
             Text("Shipping Fee", modifier = modifier.weight(1f))
             Text("$0", fontWeight = FontWeight.SemiBold)
         }
+
         Row(modifier = modifier.padding(top = 8.dp)) {
             Text("Discount", modifier = modifier.weight(1f))
             Text("$0", fontWeight = FontWeight.SemiBold)
         }
-        HorizontalDivider(modifier = modifier.padding(top = 16.dp), thickness = 1.dp)
+
+        HorizontalDivider(
+            color = colorResource(R.color.paint_04),
+            modifier = modifier.padding(top = 16.dp),
+            thickness = 1.dp
+        )
+
         Row(modifier = modifier.padding(top = 16.dp)) {
             Text("Sub Total", modifier = modifier.weight(1f))
             Text("$10003", fontWeight = FontWeight.SemiBold)
@@ -109,7 +151,7 @@ fun CartScreen(
         Button(
             onClick = {}, modifier = Modifier
                 .fillMaxWidth()
-                .padding(vertical = 16.dp)
+                .padding(16.dp)
         ) {
             Text(
                 "Pay ${if (total == 0) "" else "$$total"}",
@@ -122,7 +164,7 @@ fun CartScreen(
 
 @Preview(showBackground = true)
 @Composable
-fun HomeScreenPreview(modifier: Modifier = Modifier) {
+fun CartScreenPreview(modifier: Modifier = Modifier) {
     VShopTheme {
 //        Scaffold(modifier = modifier.fillMaxSize()) { padding ->
         CartScreen()
