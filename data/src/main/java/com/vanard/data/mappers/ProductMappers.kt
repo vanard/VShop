753f06f0ea4d38df42077541e157437a775c61e5
vanard <vianixa34@gmail.com>
2025-04-18 14:51:21 +0800

    add mapper entity

--- data/src/main/java/com/vanard/data/mappers/ProductMappers.kt
+++ data/src/main/java/com/vanard/data/mappers/ProductMappers.kt
@@ -1,5 +1,7 @@
 package com.vanard.data.mappers
 
+import com.vanard.data.entities.ProductEntity
+import com.vanard.data.entities.RatingEntity
 import com.vanard.data.remote.dto.ProductDto
 import com.vanard.data.remote.dto.RatingDto
 import com.vanard.domain.model.Product
@@ -18,6 +20,42 @@ fun ProductDto.toDomain(): Product {
 }
 
 fun RatingDto.toDomain(): Rating {
+    return Rating(this.rate, this.count)
+}
+
+//
+
+fun ProductDto.toEntity(): ProductEntity {
+    return ProductEntity(
+        this.id,
+        this.title,
+        this.price,
+        this.description,
+        this.category,
+        this.image,
+        this.rating?.toEntity()
+    )
+}
+
+fun RatingDto.toEntity(): RatingEntity {
+    return RatingEntity(this.rate, this.count)
+}
+
+//
+
+fun ProductEntity.toDomain(): Product {
+    return Product(
+        this.id,
+        this.title,
+        this.price,
+        this.description,
+        this.category,
+        this.image,
+        this.rating?.toDomain()
+    )
+}
+
+fun RatingEntity.toDomain(): Rating {
     return Rating(this.rate, this.count)
 }
 
