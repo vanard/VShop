1628afa472268e9b89f988478450312b42014bca
vanard <vianixa34@gmail.com>
2025-04-18 19:32:20 +0800

    update cart mapper

--- data/src/main/java/com/vanard/data/mappers/CartMapper.kt
+++ data/src/main/java/com/vanard/data/mappers/CartMapper.kt
@@ -1,8 +1,10 @@
 package com.vanard.data.mappers
 
+import com.vanard.data.entities.CartEntity
 import com.vanard.data.remote.dto.CartDto
 import com.vanard.data.remote.dto.ProductItemDto
 import com.vanard.domain.model.Cart
+import com.vanard.domain.model.CartItem
 import com.vanard.domain.model.ProductItem
 
 fun CartDto.toDomain(): Cart {
@@ -16,4 +18,17 @@ fun CartDto.toDomain(): Cart {
 
 fun ProductItemDto.toDomain(): ProductItem {
     return ProductItem(this.productId, this.quantity)
+}
+
+//
+fun CartEntity.toDomain(): CartItem {
+    return CartItem(
+        this.id,
+        this.date,
+        this.title,
+        this.price,
+        this.category,
+        this.image,
+        this.quantity
+    )
 }
