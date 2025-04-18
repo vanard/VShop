bf94319fd5cfe4f9a5defc9d41bb4f30ceca8888
vanard <vianixa34@gmail.com>
2025-04-18 14:51:01 +0800

    add safebody ext

--- core/common/src/main/java/com/vanard/common/util/Util.kt
+++ core/common/src/main/java/com/vanard/common/util/Util.kt
@@ -2,7 +2,17 @@ package com.vanard.common.util
 
 import android.content.Context
 import android.widget.Toast
+import retrofit2.HttpException
+import retrofit2.Response
 
 fun Context.toastMsg(msg: String) {
     Toast.makeText(this, msg, Toast.LENGTH_SHORT).show()
+}
+
+suspend fun <T> Response<T>.safeBodyOrThrow(): T {
+    if (isSuccessful) {
+        return body() ?: throw NullPointerException("Response body is null")
+    } else {
+        throw HttpException(this)
+    }
 }
