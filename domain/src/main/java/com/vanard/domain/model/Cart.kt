0fd841c40811432236d692052ced8ec3b92c2eab
vanard <vianixa34@gmail.com>
2025-04-18 19:31:35 +0800

    create cartitem

--- domain/src/main/java/com/vanard/domain/model/Cart.kt
+++ domain/src/main/java/com/vanard/domain/model/Cart.kt
@@ -1,5 +1,7 @@
 package com.vanard.domain.model
 
+import com.vanard.common.util.toIso8601Utc
+
 data class CartList(val carts: List<Cart>)
 
 data class Cart(
@@ -14,4 +16,23 @@ data class ProductItem(
     val quantity: Long,
 )
 
+data class CartItem(
+    val id: Long,
+    val date: String? = null,
+    val title: String,
+    val price: Double,
+    val category: String,
+    val image: String,
+    val quantity: Long = 1,
+)
+
 val dummyCart = Cart(0, 123123123, "", listOf(ProductItem(123123123, 2)))
+val dummyCartItem = CartItem(
+    0,
+    System.currentTimeMillis().toIso8601Utc(),
+    "Tas Banteng",
+    123123123.0,
+    "women",
+    "https://contents.mediadecathlon.com/p2691013/k\$161820d35a9a96f968f2d592788cf8f6/tas-ransel-hiking-anak-remaja-mh300-15l-pink-quechua-8739843.jpg?f=1920x0&format=auto",
+    1
+)
