2760c9ae2b3ceef68d17c806b2ce38100410505d
vanard <vianixa34@gmail.com>
2025-04-18 19:38:08 +0800

    refactor util

--- core/common/src/main/java/com/vanard/common/util/Util.kt
+++ core/common/src/main/java/com/vanard/common/util/Util.kt
@@ -8,11 +8,3 @@ import retrofit2.Response
 fun Context.toastMsg(msg: String) {
     Toast.makeText(this, msg, Toast.LENGTH_SHORT).show()
 }
-
-suspend fun <T> Response<T>.safeBodyOrThrow(): T {
-    if (isSuccessful) {
-        return body() ?: throw NullPointerException("Response body is null")
-    } else {
-        throw HttpException(this)
-    }
-}
