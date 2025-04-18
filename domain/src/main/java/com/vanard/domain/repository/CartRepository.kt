df211553b2191498cf20e875a0f2fe9b8b7b9fb6
vanard <vianixa34@gmail.com>
2025-04-18 19:32:45 +0800

    add get local cart to cart repo

--- domain/src/main/java/com/vanard/domain/repository/CartRepository.kt
+++ domain/src/main/java/com/vanard/domain/repository/CartRepository.kt
@@ -2,8 +2,10 @@ package com.vanard.domain.repository
 
 import com.vanard.common.UIState
 import com.vanard.domain.model.Cart
+import com.vanard.domain.model.CartItem
 import kotlinx.coroutines.flow.Flow
 
 interface CartRepository {
     suspend fun getAllCarts(): Flow<UIState<List<Cart>>>
+    suspend fun getAllLocalCarts(): Flow<UIState<List<CartItem>>>
 }
