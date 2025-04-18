444203e8a2f67ea5b8253ddfd8ad6322f7e6acfe
vanard <vianixa34@gmail.com>
2025-04-18 19:34:22 +0800

    add get local cart to usecase and viewmodel

--- domain/src/main/java/com/vanard/domain/usecase/CartUseCase.kt
+++ domain/src/main/java/com/vanard/domain/usecase/CartUseCase.kt
@@ -2,6 +2,7 @@ package com.vanard.domain.usecase
 
 import com.vanard.common.UIState
 import com.vanard.domain.model.Cart
+import com.vanard.domain.model.CartItem
 import com.vanard.domain.repository.CartRepository
 import kotlinx.coroutines.flow.Flow
 
@@ -9,6 +10,10 @@ class CartUseCase(private val cartRepository: CartRepository) {
 
     suspend fun getAllCarts(): Flow<UIState<List<Cart>>> {
         return cartRepository.getAllCarts()
+    }
+
+    suspend fun getAllLocalCarts(): Flow<UIState<List<CartItem>>> {
+        return cartRepository.getAllLocalCarts()
     }
 
 }
--- feature/src/main/java/com/vanard/feature/cart/CartViewModel.kt
+++ feature/src/main/java/com/vanard/feature/cart/CartViewModel.kt
@@ -1,32 +1,32 @@
 package com.vanard.feature.cart
 
 import android.util.Log
-import androidx.lifecycle.ViewModel
-import androidx.lifecycle.viewModelScope
 import com.vanard.common.UIState
+import com.vanard.domain.model.CartItem
 import com.vanard.domain.model.CartList
 import com.vanard.domain.usecase.CartUseCase
 import com.vanard.feature.home.HomeViewModel.Companion.TAG
+import com.vanard.ui.base.BaseViewModel
 import dagger.hilt.android.lifecycle.HiltViewModel
-import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.launch
-import kotlinx.coroutines.withContext
 import javax.inject.Inject
 
 @HiltViewModel
-class CartViewModel @Inject constructor(private val useCase: CartUseCase) : ViewModel() {
+class CartViewModel @Inject constructor(private val useCase: CartUseCase) : BaseViewModel() {
 
     private val _carts = MutableStateFlow(CartList(listOf()))
     val carts get() = _carts
 
+    private val _localCarts = MutableStateFlow(listOf<CartItem>())
+    val localCarts get() = _localCarts
+
     fun getCarts() {
-        viewModelScope.launch {
-//            _uiState.value = UIState.Loading
-            val result = withContext(Dispatchers.IO) {
-                useCase.getAllCarts()
-            }
-            Log.d(TAG, "getCarts: $result")
+        if (isLoaded) return
+        isLoaded = true
+
+        safeScope.launch {
+            setLoading()
             useCase.getAllCarts()
                 .collect { state ->
                     when (state) {
@@ -34,13 +34,30 @@ class CartViewModel @Inject constructor(private val useCase: CartUseCase) : View
                             val cartList = CartList(state.data)
                             Log.d(TAG, "getCarts: $cartList")
                             _carts.value = cartList
-
-//                            _uiState.value = UIState.Success(products)
+                            setSuccess()
                         }
 
                         else -> {
                             Log.d(TAG, "getCarts: error")
-//                            _uiState.value = UIState.Error("BruH")
+                        }
+                    }
+                }
+        }
+    }
+
+    fun getLocalCarts() {
+        safeScope.launch {
+            useCase.getAllLocalCarts()
+                .collect { state ->
+                    when (state) {
+                        is UIState.Success -> {
+                            val localCart = state.data
+                            Log.d(TAG, "getLocalCarts: $localCart")
+                            _localCarts.value = localCart
+                        }
+
+                        else -> {
+                            Log.d(TAG, "getCarts: error")
                         }
                     }
                 }
