5a9e3fa5b90c0bd47c47a0e291f055030dac15b6
vanard <vianixa34@gmail.com>
2025-04-18 19:32:45 +0800

    add get local cart to cart repo

--- data/src/main/java/com/vanard/data/repositoryImpl/CartRepositoryImpl.kt
+++ data/src/main/java/com/vanard/data/repositoryImpl/CartRepositoryImpl.kt
@@ -1,17 +1,22 @@
 package com.vanard.data.repositoryImpl
 
 import com.vanard.common.UIState
+import com.vanard.core.common.asUiState
 import com.vanard.core.common.fetchState
 import com.vanard.core.common.map
+import com.vanard.data.dao.CartDao
 import com.vanard.data.mappers.toDomain
 import com.vanard.data.remote.CartService
 import com.vanard.domain.model.Cart
+import com.vanard.domain.model.CartItem
 import com.vanard.domain.repository.CartRepository
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.flow.map
 import javax.inject.Inject
+import kotlin.collections.map
 
-class CartRepositoryImpl @Inject constructor(private val cartServices: CartService) :
+class CartRepositoryImpl @Inject constructor(private val cartServices: CartService, private val cartDao: CartDao) :
     CartRepository {
 
     override suspend fun getAllCarts(): Flow<UIState<List<Cart>>> = flow {
@@ -20,5 +25,11 @@ class CartRepositoryImpl @Inject constructor(private val cartServices: CartServi
             dtoList.map { it.toDomain() }
         })
     }
+
+    override suspend fun getAllLocalCarts(): Flow<UIState<List<CartItem>>> =
+        cartDao.getAllCarts().map { list ->
+            list.map { it.toDomain() }
+        }.asUiState()
+
 
 }
