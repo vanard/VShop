f0ca62912e78f68ab69519a5e0e2a90f016ad839
vanard <vianixa34@gmail.com>
2025-04-18 14:53:47 +0800

    create product dao and entity

---
+++ data/src/main/java/com/vanard/data/entities/ProductEntity.kt
@@ -0,0 +1,21 @@
+package com.vanard.data.entities
+
+import androidx.room.Embedded
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "products_item")
+data class ProductEntity(
+    @PrimaryKey val id: Long,
+    val title: String,
+    val price: Double,
+    val description: String,
+    val category: String,
+    val image: String,
+    @Embedded val rating: RatingEntity? = null,
+)
+
+data class RatingEntity(
+    val rate: Double,
+    val count: Long,
+)
