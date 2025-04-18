8d71366122871254a65c69258c91a42be316d752
vanard <vianixa34@gmail.com>
2025-04-18 19:37:31 +0800

    add get all favorite products function

--- data/src/main/java/com/vanard/data/dao/ProductDao.kt
+++ data/src/main/java/com/vanard/data/dao/ProductDao.kt
@@ -5,6 +5,7 @@ import androidx.room.Delete
 import androidx.room.Insert
 import androidx.room.OnConflictStrategy
 import androidx.room.Query
+import androidx.room.Update
 import com.vanard.data.entities.ProductEntity
 import kotlinx.coroutines.flow.Flow
 
@@ -17,11 +18,17 @@ interface ProductDao {
     @Insert(onConflict = OnConflictStrategy.REPLACE)
     suspend fun insertAllProduct(listProductEntity: List<ProductEntity>)
 
+    @Update
+    suspend fun updateProduct(product: ProductEntity)
+
     @Query("SELECT * FROM products_item WHERE id = :id")
     fun getProductById(id:Long): Flow<ProductEntity?>
 
     @Query("SELECT * FROM products_item")
     fun getAllProducts(): Flow<List<ProductEntity>>
+
+    @Query("SELECT * FROM products_item WHERE isFavorite = 1")
+    fun getAllFavoriteProducts(): Flow<List<ProductEntity>>
 
     @Delete
     suspend fun deleteProduct(productEntity: ProductEntity)
