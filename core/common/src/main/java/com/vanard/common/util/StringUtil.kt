16c6571b6f1c2b6096782159fd69670f7c8e9f77
vanard <vianixa34@gmail.com>
2025-04-24 20:19:08 +0800

    update util

--- core/common/src/main/java/com/vanard/common/util/StringUtil.kt
+++ core/common/src/main/java/com/vanard/common/util/StringUtil.kt
@@ -4,3 +4,6 @@ fun String.firstWord(): String = this.split(" ").firstOrNull() ?: ""
 
 fun String.firstWords(count: Int): String =
     this.split(" ").take(count).joinToString(" ")
+
+fun String.maxDecimal(count: Int, number: Double): String =
+    this.format("%.${count}f", number)
--- core/common/src/main/java/com/vanard/common/util/Util.kt
+++ core/common/src/main/java/com/vanard/common/util/Util.kt
@@ -4,7 +4,11 @@ import android.content.Context
 import android.widget.Toast
 import retrofit2.HttpException
 import retrofit2.Response
+import kotlin.math.roundToInt
 
 fun Context.toastMsg(msg: String) {
     Toast.makeText(this, msg, Toast.LENGTH_SHORT).show()
 }
+
+fun Double.formalDecimal() =
+    (this * 100).roundToInt() / 100.0
