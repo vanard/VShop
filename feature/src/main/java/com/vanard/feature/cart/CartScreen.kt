39be9ac7953465f481079ee0c0d0cece74282f88
vanard <vianixa34@gmail.com>
2025-04-24 20:21:30 +0800

    refactor cartUseCase, now using only productUseCase, add calculate, update, delete cart

--- feature/src/main/java/com/vanard/feature/cart/CartScreen.kt
+++ feature/src/main/java/com/vanard/feature/cart/CartScreen.kt
@@ -1,5 +1,6 @@
 package com.vanard.feature.cart
 
+import android.util.Log
 import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
@@ -9,7 +10,7 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.lazy.itemsIndexed
 import androidx.compose.foundation.lazy.rememberLazyListState
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material3.Button
@@ -21,6 +22,9 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalConfiguration
@@ -34,12 +38,17 @@ import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.hilt.navigation.compose.hiltViewModel
 import com.vanard.common.UIState
+import com.vanard.common.util.formalDecimal
 import com.vanard.common.util.toastMsg
-import com.vanard.feature.ComingSoonScreen
+import com.vanard.feature.ErrorScreen
 import com.vanard.resources.R
 import com.vanard.ui.components.CartItemContent
+import com.vanard.ui.components.CountViewActions
 import com.vanard.ui.components.NormalText
+import com.vanard.ui.components.RemoveMenuActions
 import com.vanard.ui.theme.VShopTheme
+
+private const val TAG = "CartScreen"
 
 @Composable
 fun CartScreen(
@@ -50,7 +59,7 @@ fun CartScreen(
     viewModel.uiState.collectAsState().value.let { uiState ->
         when (uiState) {
             is UIState.Loading -> {
-                viewModel.getCarts()
+//                viewModel.getCarts()
                 viewModel.getLocalCarts()
 
                 Column(
@@ -65,15 +74,18 @@ fun CartScreen(
             }
 
             is UIState.Success -> {
-                var total = 0.0
+//                var total by remember { mutableDoubleStateOf(0.0) }
 
                 val configuration = LocalConfiguration.current
                 val screenHeight = configuration.screenHeightDp.dp
 
-                val context = LocalContext.current
-//    val carts by viewModel.carts.collectAsState()
-                val carts by viewModel.localCarts.collectAsState()
+//                val context = LocalContext.current
+                val total by viewModel.total.collectAsState()
+//                val carts by viewModel.carts.collectAsState()
+                val localCarts by viewModel.localCarts.collectAsState()
                 val scrollState = rememberLazyListState()
+
+                var expandedIndex by remember { mutableStateOf<Int?>(null) }
 
                 Column(
                     modifier = modifier
@@ -86,26 +98,64 @@ fun CartScreen(
                         state = scrollState,
                         modifier = modifier.size((screenHeight / 2) - 36.dp),
                         content = {
-                            items(
+                            itemsIndexed(
 //                    items = carts.carts,
-                                items = carts,
-                                key = { it.id }) { cart ->
+                                items = localCarts,
+                                key = { index, _ -> index }) { index, cart ->
+                                val menuActions = RemoveMenuActions(
+                                    onMenuClick = {
+                                        expandedIndex = index
+                                        Log.d(TAG, "onMenuClick: ${expandedIndex == index}")
+                                    },
+                                    showMenu = expandedIndex == index,
+                                    onDismissMenu = {
+                                        expandedIndex = null
+                                        Log.d(TAG, "onDismissMenu: ${expandedIndex == index}")
+                                    },
+                                    onRemoveItem = {
+                                        viewModel.removeCartItem(cart)
+                                        Log.d(TAG, "onRemoveItem: ${expandedIndex == index}")
+                                    }
+                                )
+
+                                val countActions = CountViewActions(
+                                    onPlusClick = { quantity ->
+                                        viewModel.updateCartItem(cart.apply {
+                                            quantityInCart = quantity
+                                        })
+                                        viewModel.calculateTotal()
+                                        Log.d(TAG, "PlusClick: $quantity : ${cart.quantityInCart}")
+                                    },
+                                    onMinusClick = { quantity ->
+                                        viewModel.updateCartItem(cart.apply {
+                                            quantityInCart = quantity
+                                        })
+                                        viewModel.calculateTotal()
+                                        Log.d(TAG, "MinusClick: $quantity : ${cart.quantityInCart}")
+                                    }
+                                )
+
                                 CartItemContent(
                                     cart,
                                     onSelectedProduct = {
-                                        context.toastMsg("Product Cart")
+//                                        context.toastMsg("Product Cart")
                                     },
+                                    menuActions = menuActions,
+                                    countActions = countActions,
                                 )
+
                             }
 
                         })
 
-                    FooterCartScreen(total)
+                    FooterCartScreen(
+                        total = total
+                    )
                 }
             }
 
             is UIState.Error -> {
-                ComingSoonScreen()
+                ErrorScreen()
             }
         }
     }
@@ -113,6 +163,7 @@ fun CartScreen(
 
 @Composable
 fun HeaderCartScreen(navigateBack: () -> Unit, modifier: Modifier = Modifier) {
+    val context = LocalContext.current
     Row(
         verticalAlignment = Alignment.CenterVertically,
         modifier = modifier.padding(vertical = 16.dp)
@@ -142,7 +193,9 @@ fun HeaderCartScreen(navigateBack: () -> Unit, modifier: Modifier = Modifier) {
                 .weight(1f)
         )
 
-        IconButton(onClick = {}) {
+        IconButton(onClick = {
+            context.toastMsg("Coming Soon")
+        }) {
             Icon(
                 painter = painterResource(R.drawable.menu),
                 contentDescription = null,
@@ -169,17 +222,17 @@ fun FooterCartScreen(total: Double, modifier: Modifier = Modifier) {
 
     Row(modifier = modifier.padding(top = 12.dp)) {
         NormalText("Total", modifier = modifier.weight(1f))
-        NormalText("$10003", fontWeight = FontWeight.SemiBold)
+        NormalText("$${total.formalDecimal()}", fontWeight = FontWeight.SemiBold)
     }
 
     Row(modifier = modifier.padding(top = 8.dp)) {
         NormalText("Shipping Fee", modifier = modifier.weight(1f))
-        NormalText("$0", fontWeight = FontWeight.SemiBold)
+        NormalText("$0", fontWeight = FontWeight.SemiBold) // TODO
     }
 
     Row(modifier = modifier.padding(top = 8.dp)) {
         NormalText("Discount", modifier = modifier.weight(1f))
-        NormalText("$0", fontWeight = FontWeight.SemiBold)
+        NormalText("$0", fontWeight = FontWeight.SemiBold) // TODO
     }
 
     HorizontalDivider(
@@ -190,7 +243,7 @@ fun FooterCartScreen(total: Double, modifier: Modifier = Modifier) {
 
     Row(modifier = modifier.padding(top = 12.dp, bottom = 8.dp)) {
         NormalText("Sub Total", modifier = modifier.weight(1f))
-        NormalText("$10003", fontWeight = FontWeight.SemiBold)
+        NormalText("$${total.formalDecimal()}", fontWeight = FontWeight.SemiBold) // TODO
     }
 
     Button(
@@ -201,7 +254,7 @@ fun FooterCartScreen(total: Double, modifier: Modifier = Modifier) {
             .padding(16.dp)
     ) {
         Text(
-            "Pay ${if (total == 0.0) "" else "$$total"}",
+            "Pay ${if (total == 0.0) "" else "$${total.formalDecimal()}"}",
             fontSize = 18.sp,
             color = colorResource(R.color.paint_04),
             modifier = modifier.padding(vertical = 16.dp)
--- feature/src/main/java/com/vanard/feature/cart/CartViewModel.kt
+++ feature/src/main/java/com/vanard/feature/cart/CartViewModel.kt
@@ -2,51 +2,57 @@ package com.vanard.feature.cart
 
 import android.util.Log
 import com.vanard.common.UIState
-import com.vanard.domain.model.CartItem
-import com.vanard.domain.model.CartList
-import com.vanard.domain.usecase.CartUseCase
-import com.vanard.feature.home.HomeViewModel.Companion.TAG
+import com.vanard.domain.model.Product
+import com.vanard.domain.usecase.ProductUseCase
 import com.vanard.ui.base.BaseViewModel
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
-class CartViewModel @Inject constructor(private val useCase: CartUseCase) : BaseViewModel() {
+class CartViewModel @Inject constructor(
+    private val useCase: ProductUseCase,
+//    private val cartUseCase: CartUseCase,
+) : BaseViewModel() {
 
-    private val _carts = MutableStateFlow(CartList(listOf()))
-    val carts get() = _carts
+//    private val _carts = MutableStateFlow(CartList(listOf()))
+//    val carts get() = _carts.asStateFlow()
 
-    private val _localCarts = MutableStateFlow(listOf<CartItem>())
-    val localCarts get() = _localCarts
+    private val _total = MutableStateFlow(0.0)
+    val total get() = _total.asStateFlow()
 
-    fun getCarts() {
-        if (isLoaded) return
-        isLoaded = true
+    private val _localCarts = MutableStateFlow(listOf<Product>())
+    val localCarts get() = _localCarts.asStateFlow()
 
-        safeScope.launch {
-            setLoading()
-            useCase.getAllCarts()
-                .collect { state ->
-                    when (state) {
-                        is UIState.Success -> {
-                            val cartList = CartList(state.data)
-                            Log.d(TAG, "getCarts: $cartList")
-                            _carts.value = cartList
-                            setSuccess()
-                        }
-
-                        else -> {
-                            Log.d(TAG, "getCarts: error")
-                        }
-                    }
-                }
-        }
-    }
+//    fun getCarts() {
+//        if (isLoaded) return
+//        isLoaded = true
+//
+//        safeScope.launch {
+//            setLoading()
+//            cartUseCase.getAllCarts()
+//                .collect { state ->
+//                    when (state) {
+//                        is UIState.Success -> {
+//                            val cartList = CartList(state.data)
+//                            Log.d(TAG, "getCarts: $cartList")
+//                            _carts.value = cartList
+//                            setSuccess()
+//                        }
+//
+//                        else -> {
+//                            Log.d(TAG, "getCarts: error")
+//                        }
+//                    }
+//                }
+//        }
+//    }
 
     fun getLocalCarts() {
         safeScope.launch {
+            setLoading()
             useCase.getAllLocalCarts()
                 .collect { state ->
                     when (state) {
@@ -54,9 +60,12 @@ class CartViewModel @Inject constructor(private val useCase: CartUseCase) : Base
                             val localCart = state.data
                             Log.d(TAG, "getLocalCarts: $localCart")
                             _localCarts.value = localCart
+                            calculateTotal()
+                            setSuccess()
                         }
 
                         else -> {
+                            setError("Error")
                             Log.d(TAG, "getCarts: error")
                         }
                     }
@@ -64,10 +73,28 @@ class CartViewModel @Inject constructor(private val useCase: CartUseCase) : Base
         }
     }
 
-    fun calculateTotal() {
-        _carts.value.carts.forEach { cart ->
-            cart.products.first()
+    fun updateCartItem(cart: Product) {
+        safeScope.launch {
+            useCase.updateProduct(cart)
+            Log.d(TAG, "updateCartItem: ${_localCarts.value[_localCarts.value.indexOf(cart)]}")
         }
+    }
+
+    fun removeCartItem(cart: Product) {
+        safeScope.launch {
+            useCase.updateProduct(cart.apply { quantityInCart = 0 })
+            Log.d(TAG, "updateCartItem: ${_localCarts.value[_localCarts.value.indexOf(cart)]}")
+        }
+    }
+
+    fun calculateTotal() {
+        _total.value = _localCarts.value.sumOf { cart ->
+            cart.price * cart.quantityInCart
+        }
+    }
+
+    companion object {
+        private const val TAG = "CartViewModel"
     }
 
 }
