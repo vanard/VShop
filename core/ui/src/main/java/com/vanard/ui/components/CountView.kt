31be58c3f9a525de8736aec1beea203a6a425a36
vanard <vianixa34@gmail.com>
2025-04-24 20:22:03 +0800

    update count view, now add function onPlus and onMinus

--- core/ui/src/main/java/com/vanard/ui/components/CountView.kt
+++ core/ui/src/main/java/com/vanard/ui/components/CountView.kt
@@ -1,5 +1,6 @@
 package com.vanard.ui.components
 
+import android.util.Log
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Arrangement
@@ -11,6 +12,10 @@ import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableIntStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
@@ -22,12 +27,14 @@ import com.vanard.resources.R
 
 @Composable
 fun CountView(
-    onPlusClick: () -> Unit,
-    onMinusClick: () -> Unit,
+    currentQuantity: Int,
+    onPlusClick: (Int) -> Unit,
+    onMinusClick: (Int) -> Unit,
     customIconSize: Int = 24,
     modifier: Modifier = Modifier,
 ) {
     val iconSize = customIconSize - 12
+    var quantity by remember { mutableIntStateOf(currentQuantity) }
 
     Row(
         verticalAlignment = Alignment.CenterVertically,
@@ -36,7 +43,12 @@ fun CountView(
             .padding(top = 16.dp)
     ) {
         IconButton(
-            onClick = onMinusClick, modifier = Modifier
+            onClick = {
+                if (quantity > 1)
+                    quantity--
+                onMinusClick(quantity)
+                Log.d(TAG, "Plus Click: $quantity")
+            }, modifier = Modifier
                 .padding(8.dp)
                 .border(1.dp, color = colorResource(R.color.paint_03), shape = CircleShape)
                 .clip(CircleShape)
@@ -51,10 +63,14 @@ fun CountView(
             )
         }
 
-        Text("0", modifier = modifier.padding(start = 4.dp))
+        Text("$quantity", modifier = modifier.padding(start = 4.dp))
 
         IconButton(
-            onClick = onPlusClick, modifier = Modifier
+            onClick = {
+                quantity++
+                onPlusClick(quantity)
+                Log.d(TAG, "Plus Click: $quantity")
+            }, modifier = Modifier
                 .padding(8.dp)
                 .border(1.dp, color = colorResource(R.color.paint_03), shape = CircleShape)
                 .clip(CircleShape)
@@ -71,3 +87,5 @@ fun CountView(
 
     }
 }
+
+private const val TAG = "CountView"
