0ae73fcc3877fe6e8220a19bbf9bbe618d45c18a
vanard <vianixa34@gmail.com>
2025-05-01 21:03:18 +0800

    update compose rule ext

--- app/src/androidTest/java/com/vanard/vshop/util/ComposeRuleExt.kt
+++ app/src/androidTest/java/com/vanard/vshop/util/ComposeRuleExt.kt
@@ -1,9 +1,11 @@
 package com.vanard.vshop.util
 
+import android.content.Context
 import androidx.activity.ComponentActivity
 import androidx.annotation.StringRes
 import androidx.compose.ui.test.SemanticsNodeInteraction
 import androidx.compose.ui.test.junit4.AndroidComposeTestRule
+import androidx.compose.ui.test.junit4.ComposeContentTestRule
 import androidx.compose.ui.test.onNodeWithText
 import androidx.test.ext.junit.rules.ActivityScenarioRule
 
@@ -14,3 +16,14 @@ fun <A : ComponentActivity> AndroidComposeTestRule<ActivityScenarioRule<A>, A>.o
 fun <A : ComponentActivity> AndroidComposeTestRule<ActivityScenarioRule<A>, A>.onNodeWithStringId(
     @StringRes id: Int, vararg args: Any,
 ): SemanticsNodeInteraction = onNodeWithText(activity.getString(id, args))
+
+fun ComposeContentTestRule.onNodeWithStringId(
+    context: Context,
+    @StringRes id: Int
+): SemanticsNodeInteraction = onNodeWithText(context.getString(id))
+
+fun ComposeContentTestRule.onNodeWithStringId(
+    context: Context,
+    @StringRes id: Int,
+    vararg args: Any,
+): SemanticsNodeInteraction = onNodeWithText(context.getString(id, args))
