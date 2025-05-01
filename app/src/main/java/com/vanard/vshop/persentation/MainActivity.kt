b9e2d0edde3f7077ba5b529e88f7c378f0abb961
vanard <vianixa34@gmail.com>
2025-05-01 21:03:04 +0800

    try to handle nav controller on test

--- app/src/main/java/com/vanard/vshop/persentation/MainActivity.kt
+++ app/src/main/java/com/vanard/vshop/persentation/MainActivity.kt
@@ -4,19 +4,29 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.runtime.remember
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.rememberNavController
 import com.vanard.ui.theme.VShopTheme
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
+    var navControllerProvider: (() -> NavHostController)? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         installSplashScreen()
         enableEdgeToEdge()
+
         setContent {
             VShopTheme {
-                VShopApp()
+                val navController = navControllerProvider?.invoke() ?: rememberNavController()
+                val appState = remember(navController) {
+                    VShopState(navController)
+                }
+                VShopApp(appState)
             }
         }
     }
