9e0f81441934d7adfddfd40d1c3fe781306770e3
vanard <vianixa34@gmail.com>
2025-05-10 15:39:52 +0800

    add gradle settings

---
+++ settings.gradle.kts
@@ -0,0 +1,30 @@
+pluginManagement {
+    repositories {
+        google {
+            content {
+                includeGroupByRegex("com\\.android.*")
+                includeGroupByRegex("com\\.google.*")
+                includeGroupByRegex("androidx.*")
+            }
+        }
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "VShop"
+include(":app")
+include(":core")
+include(":core:resources")
+include(":data")
+include(":feature")
+include(":domain")
+include(":core:common")
+include(":core:ui")
