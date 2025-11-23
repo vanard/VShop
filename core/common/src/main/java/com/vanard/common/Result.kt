556e6849430365c5c8f61c4215090dae6c615ee4
vanard <vianixa34@gmail.com>
2025-11-23 21:00:45 +0700

    add result to handle domain logic

---
+++ core/common/src/main/java/com/vanard/common/Result.kt
@@ -0,0 +1,46 @@
+package com.vanard.common
+
+sealed class Result<out T> {
+    data class Success<out T>(val data: T) : Result<T>()
+    data class Error(val exception: Exception) : Result<Nothing>()
+    object Loading : Result<Nothing>()
+}
+
+inline fun <T> Result<T>.onSuccess(action: (value: T) -> Unit): Result<T> {
+    if (this is Result.Success) action(data)
+    return this
+}
+
+inline fun <T> Result<T>.onError(action: (exception: Exception) -> Unit): Result<T> {
+    if (this is Result.Error) action(exception)
+    return this
+}
+
+inline fun <T> Result<T>.onLoading(action: () -> Unit): Result<T> {
+    if (this is Result.Loading) action()
+    return this
+}
+
+inline fun <T, R> Result<T>.map(transform: (T) -> R): Result<R> {
+    return when (this) {
+        is Result.Success -> Result.Success(transform(data))
+        is Result.Error -> this
+        is Result.Loading -> this
+    }
+}
+
+inline fun <T, R> Result<T>.flatMap(transform: (T) -> Result<R>): Result<R> {
+    return when (this) {
+        is Result.Success -> transform(data)
+        is Result.Error -> this
+        is Result.Loading -> this
+    }
+}
+
+fun <T> Result<T>.toUIState(): UIState<T> {
+    return when (this) {
+        is Result.Success -> UIState.Success(data)
+        is Result.Error -> UIState.Error(exception.message ?: "Unknown error")
+        is Result.Loading -> UIState.Loading
+    }
+}
